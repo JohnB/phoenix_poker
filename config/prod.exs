2ec96462f220fd91ddafad474fdecd56d5f5dd9a
JohnB <john.baylor@gmail.com>
2016-10-09 14:07:01 -0700

    Ignore /junk dir. Consider "debug_errors" in prod.

--- config/prod.exs
+++ config/prod.exs
@@ -3,6 +3,7 @@ use Mix.Config
 config :phoenix_poker, PhoenixPoker.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [scheme: "https", host: "morning-river-32535.herokuapp.com", port: 443],
+  # debug_errors: true,
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
