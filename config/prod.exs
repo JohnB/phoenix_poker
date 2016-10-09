e02be8f4ee63f196fc9292989aed8c40b8200b5f
JohnB <john.baylor@gmail.com>
2016-10-09 10:17:19 -0700

    Add heroku support, per http://www.phoenixframework.org/docs/heroku   heroku create --buildpack "https://github.com/HashNuke/heroku-buildpack-elixir.git"   heroku buildpacks:add https://github.com/gjaldon/heroku-buildpack-phoenix-static.git   heroku addons:create heroku-postgresql:hobby-dev   heroku config:set POOL_SIZE=18   heroku config:set SECRET_KEY_BASE="`mix phoenix.gen.secret`"

--- config/prod.exs
+++ config/prod.exs
@@ -2,7 +2,7 @@ use Mix.Config
 
 config :phoenix_poker, PhoenixPoker.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "ueberauth-example.herokuapp.com", port: 443],
+  url: [scheme: "https", host: "morning-river-32535.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
@@ -14,4 +14,5 @@ config :logger, level: :info
 config :phoenix_poker, PhoenixPoker.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
-  pool_size: 20
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
