303ad4793d8869e69b738cc21ee2f0c0be1914de
JohnB <john.baylor@gmail.com>
2016-10-14 00:15:54 -0700

    Connected future games to the home page. TODO: try fetching Players instead of tuples. Experimented with nickname radio buttons.

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -4,8 +4,27 @@ defmodule PhoenixPoker.PageController do
   """
 
   use PhoenixPoker.Web, :controller
+  import Ecto.Query, only: [from: 2]
 
   def index(conn, _params) do
-    render conn, "index.html", current_user: get_session(conn, :current_user)
+    
+    yyyymmdd = DateTime.utc_now.year * 10000 + DateTime.utc_now.month * 100 + DateTime.utc_now.day
+
+    future = from( g in "game_nights",
+             where: g.yyyymmdd > ^yyyymmdd,
+             select: {g.id, g.yyyymmdd} )
+#    current = from g in "game_nights",
+#              where: g.yyyymmdd = yyyymmdd,
+#              select: g.id, g.yyyymmdd
+#    past = from g in "game_nights",
+#           where: g.yyyymmdd < yyyymmdd,
+#           select: g.id, g.yyyymmdd
+              
+    render( conn, "index.html",
+      current_user: get_session(conn, :current_user),
+      future: Repo.all(future) #,
+#      current: Repo.all(current),
+#      past: Repo.all(past)
+    )
   end
 end
