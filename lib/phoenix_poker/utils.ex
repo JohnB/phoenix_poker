e9423eb46f1eb13700bc287ead7caf241ec073a4
JohnB <john.baylor@gmail.com>
2016-12-03 12:00:41 -0800

    Order email results winnings DESC and then names ASC. TODO: a "totals" row at the bottom, just like on the cashout page. TODO: connect to a mailer so Jay doesn't care.

--- lib/phoenix_poker/utils.ex
+++ lib/phoenix_poker/utils.ex
@@ -20,16 +20,26 @@ defmodule PhoenixPoker.Utils do
     end)
     |> Enum.join(",")
     
-    body = Enum.map(game_night.attendee_results, fn(a_r) ->
-      " #{a_r.player.nickname}: $#{
-        round(a_r.rounded_cents / 100)} (#{
-        a_r.chips / 100} chips / $#{
-        a_r.exact_cents / 100}) <br /> "
-    end)
+    body = Enum.sort(game_night.attendee_results, &(chips_n_name(&1) < chips_n_name(&2)) )
+    |> Enum.map(fn(a_r) -> email_row(a_r) end)
     |> Enum.join("\n")
 
     "mailto:" <> emails <>
     "?subject=" <> subj <>
     "&body=" <> body <> ";"
   end
+  
+  # To get chips ordered *descending* but names *ascending* we subtract
+  # from a huge number so we can sort it all *ascending*.
+  def chips_n_name(a_r) do
+    "#{1_000_000 - a_r.chips} #{a_r.player.nickname}"
+  end
+  
+  def email_row(a_r) do
+    " #{a_r.player.nickname}: $#{
+      round(a_r.rounded_cents / 100)} (#{
+      a_r.chips / 100} chips / $#{
+      a_r.exact_cents / 100}) <br /> "
+  end
+
 end
