0cb0c66a8d169f72788e5f295f061e57d2b54d58
JohnB <john.baylor@gmail.com>
2017-01-07 23:58:24 -0800

    Clarify when something is only admin-visible. (or, more precisely, scaffold-level-visible)

--- lib/phoenix_poker/login_helper.ex
+++ lib/phoenix_poker/login_helper.ex
@@ -3,10 +3,17 @@ defmodule PhoenixPoker.LoginHelper do
 
   def current_user(conn) do
     get_session(conn, :current_user)
-#    Guardian.Plug.current_resource(conn)
+    # Why is this commented out?
+    # Guardian.Plug.current_resource(conn)
   end
 
-#  def logged_in?(conn) do
-#    Guardian.Plug.authenticated?(conn)
-#  end
+  # The scaffolding CRUD UI is very powerful, so don't link
+  # to it unless we should have that power.
+  def can_access_scaffolding?(conn) do
+    (current_user(conn).name == "John Baylor")
+  end
+
+  #  def logged_in?(conn) do
+  #    Guardian.Plug.authenticated?(conn)
+  #  end
 end
